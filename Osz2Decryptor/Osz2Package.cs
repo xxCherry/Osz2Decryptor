580766c95781aa774e1a838f0656eb686688d321
xxCherry <aazetthr@gmail.com>
2021-02-07 10:17:21 +0300

    Revert "File reader fix. Thanks to @Karoo13"
    This reverts commit 23ea5e35ca38f7dd44880aa7f01b67d3e7770f94.

--- Osz2Decryptor/Osz2Package.cs
+++ Osz2Decryptor/Osz2Package.cs
@@ -21,7 +21,7 @@ namespace Osz2Decryptor
         public Dictionary<string, FileInfo> FileInfos = new ();
         
         /// <summary>
-        /// A dictionary that contains osz2 file contents
+        /// A dictionary that contains .osu file contents
         /// </summary>
         public Dictionary<string, byte[]> Files = new ();
 
@@ -222,10 +222,10 @@ namespace Osz2Decryptor
 
                             var nextOffset = 0;
 
-                            if (i + 1 < count)
+                            if (i++ < count)
                                 nextOffset = fileReader.ReadInt32();
                             else
-                                nextOffset = (int) reader.BaseStream.Length - fileOffset;
+                                nextOffset = (int) fileReader.BaseStream.Length - fileOffset;
 
                             var fileLength = nextOffset - currentOffset;
 
@@ -241,14 +241,7 @@ namespace Osz2Decryptor
                         using var osz2Stream = new Osz2Stream(reader.BaseStream, fileOffset + value.Offset, _key);
                         using var osz2Reader = new BinaryReader(osz2Stream);
 
-                        try
-                        {
-                            Files.Add(key, osz2Reader.ReadBytes(value.Size - 4));
-                        }
-                        catch
-                        {
-                            Console.WriteLine("Failed to read: " + key);
-                        }
+                        Files.Add(key, osz2Reader.ReadBytes(value.Size));
                     }
                 }
             }
--- Osz2DecryptorTest/Program.cs
+++ Osz2DecryptorTest/Program.cs
@@ -9,16 +9,9 @@ namespace Osz2DecryptorTest
     {
         static void Main(string[] args)
         {
-            var path = "../../../osz2/864877.osz2";
+            var path = "../../../osz2/nekodex - welcome to christmas!.osz2";
             var osz2 = new Osz2Package(path);
-            foreach (var file in osz2.Files)
-            {
-                Console.WriteLine(file);
-            }
-
-            Console.WriteLine(osz2.Files.Count);
-
-            var osuFile = osz2.Files.ToArray()[12];
+            var osuFile = osz2.Files.ToArray()[0];
             
             File.WriteAllBytes($"../../../osz2/{osuFile.Key}-unpacked", osuFile.Value);
             
